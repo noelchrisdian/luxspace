77abae5b0d0124eb579418ae3ee168d92bd9b563
noelchrisdian <noelchrisdian@gmail.com>
2025-06-18 21:52:14 +0700

    Added BrowseRoom, Clients, and Sitemap components

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,21 @@
+import { BrowseRoom } from "../parts/BrowseRoom";
+import { Clients } from "../parts/Clients";
+import { Footer } from "../parts/Footer";
 import { Header } from "../parts/Header";
 import { Hero } from "../parts/Hero";
 import { JustArrived } from "../parts/JustArrived";
+import { Sitemap } from "../parts/Sitemap";
 
 const Homepage = () => {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<BrowseRoom />
 			<JustArrived />
+			<Clients />
+			<Sitemap />
+			<Footer />
 		</>
 	)
 }
